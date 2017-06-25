849792d5b42b73a1b7e16abd790b7e6edab53a11
GavinMulvany <gavinmulvany96@gmail.com>
2017-06-25 21:08:38 +0100

    Update: ImageButton of drink has been placed on screen and a GO button has also be placed. Starting code now

--- app/src/main/java/com/example/gavinmulvany/bottlegameapp/BottleActivity.java
+++ app/src/main/java/com/example/gavinmulvany/bottlegameapp/BottleActivity.java
@@ -2,12 +2,22 @@ package com.example.gavinmulvany.bottlegameapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class BottleActivity extends AppCompatActivity {
+
+    ImageButton ib_bottle;
+    Button b_go;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottle);
+
+        ib_bottle = (ImageButton) findViewById(R.id.ib_bottle);
+        b_go = (Button) findViewById(R.id.b_go);
+
     }
 }
